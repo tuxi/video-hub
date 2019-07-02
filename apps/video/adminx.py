297484024950e1d134ad215e19cafda60ad1c3fe
tuxi <seyooe@gmail.com>
2019-07-02 19:45:18 +0800

    Add adminx for video app.

---
+++ apps/video/adminx.py
@@ -0,0 +1,18 @@
+import xadmin
+from .models import Video, HotSearchWords
+
+class VideoAdmin(object):
+    list_display = ["content", "cover_duration", "cover_start_second", "video", "longitude", "latitude" , "poi_name", "poi_address", "first_create_time", "source",]
+    search_fields = ['content', ]
+    list_editable = ["is_hot", ]
+    list_filter = ["content", "click_num", "is_hot", "first_create_time", "upload_time"]
+    style_fields = {"goods_desc": "ueditor"}
+
+
+class HotSearchAdmin(object):
+    list_display = ["keywords", "index", "add_time"]
+
+
+xadmin.site.register(Video, VideoAdmin)
+xadmin.site.register(HotSearchWords, HotSearchAdmin)
+
